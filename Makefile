156c85eccb7293b411d7daaa2d764b282177a926
Ховалыг Тараа Эресович <TEKhovalyg@sberbank.ru>
2023-03-03 23:44:35 +0300

    some fix

--- Makefile
+++ Makefile
@@ -3,12 +3,16 @@
 all: build test
 
 build:
-	GOPROXY=direct go get -d ./...
+	go get -d ./...
 	go build -o ./bin/music_service
+
 test:
 	go test ./...
+
 docker:
 	docker-compose up -d --build
+	docker logs -tf music_service
+	
 proto:
 	protoc --go_out=. --go_opt=paths=import \
     --go-grpc_out=. --go-grpc_opt=paths=import \
--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	connStr := "user=gocloud password=gocloud dbname=playlist sslmode=disable host=0.0.0.0 port=5432"
+	connStr := "user=gocloud password=gocloud dbname=playlist sslmode=disable host=192.168.1.3 port=5432"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic("DB connection error")
@@ -19,50 +19,14 @@ func main() {
 	defer db.Close()
 
 	grpcServer := grpc.NewServer()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9000))
+	lis, err := net.Listen("tcp", "192.168.1.2:9000")
 	if err != nil {
-        panic(err)
+		fmt.Printf("listen error")
+        panic("listen error")
     }
 	api.RegisterMusicServiceServer(grpcServer, service.NewService(db))
 	if err := grpcServer.Serve(lis); err != nil {
 		panic(err)
 	}
-	// playlist.PushBack(&core.Song{Name: "Blue bird", Duration: 40})
-	// playlist.PushBack(&core.Song{Name: "All of you", Duration: 120})
-	// p := core.CreateSimplePlaylist("My favorite playlist", playlist, context.Background())
-	// var act string
 
-	// for act != "stop" {
-	// 	// fmt.Scanln(&act)
-	// 	scanner  := bufio.NewScanner(os.Stdin)
-	// 	scanner.Split(bufio.ScanLines)
-	// 	scanner.Scan()
-	// 	act = scanner.Text()
-	// 	if act == "play" {
-	// 		fmt.Println("Playing")
-	// 		p.Play()
-	// 	} else if act == "pause" {
-	// 		fmt.Println("Paused")
-	// 		p.Pause()
-	// 	} else if act == "prev" {
-	// 		fmt.Println("Prev song")
-	// 		p.Prev()
-	// 	} else if act == "next" {
-	// 		fmt.Println("Next song")
-	// 		p.Next()
-	// 	} else if strings.Contains(act, "add") {
-	// 		params := strings.Split(act, " ")
-	// 		if (len(params) != 3 || params[0] != "add") {
-	// 			fmt.Println("can't add new song, wrong format. Use: add <songName> <duration>")
-	// 			continue
-	// 		}
-	// 		dur, err := strconv.Atoi(params[2])
-	// 		if err != nil {
-	// 			panic("Atoi error")
-	// 		}
-	// 		song := &core.Song{Name: params[1], Duration: dur}
-	// 		fmt.Printf("New song added: %s(%ds)\n", song.Name, song.Duration)
-	// 		p.AddSong(song)
-	// 	}
-	// }
 }
